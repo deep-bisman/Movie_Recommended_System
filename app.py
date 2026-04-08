144b5a6c17a6f7dc57f9fc0f971ffa7f8244b6b2
deep-bisman <deepbisman99@gmail.com>
2026-04-08 11:58:00 +0530

    fix render issue

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import os
 file_path = "similarity.pkl"
 
 if not os.path.exists(file_path):
-    file_id = "1C-34ly38iRPuInp9mAj1HyB4IHzwCA-B"
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, file_path, quiet=False,fuzzy=True)
+    url = "https://drive.google.com/file/d/1C-34ly38iRPulnp9mAj1HyB4lHzWCA-B/view?usp=sharing"
+
+    gdown.download(url, file_path, quiet=False)
 
 similarity = pickle.load(open('similarity.pkl','rb'))
 
